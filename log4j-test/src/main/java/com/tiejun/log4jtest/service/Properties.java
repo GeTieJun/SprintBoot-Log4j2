dcbb018315d3e3fc39ee59fef1d5a4e65b6a44ae
tiejun.ge <1354507073@qq.com>
2019-02-12 18:20:12 +0800

    Fix: 屏蔽springboot自带logback jar，以及新增测试异常处理

--- log4j-test/src/main/java/com/tiejun/log4jtest/service/Properties.java
+++ log4j-test/src/main/java/com/tiejun/log4jtest/service/Properties.java
@@ -1,15 +1,21 @@
 package com.tiejun.log4jtest.service;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+// 将配置文件的值注入到某一个实体类中方式分为两种
+// 1.单个值进行注入，在applicaton.properties中单独写入 com.tiejun.properties.password=123456789 
+//   在实体类每个字段上使用@Value("${com.tiejun.properties.password}")进行注入
+// 2.基于类型安全的配置，在applicaton.properties中单独写入user.password=123456789
+//   在实体类上面
 @Component
+@ConfigurationProperties (prefix="user")
 public class Properties {
 	
-	@Value("${com.tiejun.properties.userName}")
+	// @Value("${com.tiejun.properties.userName}")
 	private String userName;
 	
-	@Value("${com.tiejun.properties.password}")
+	// @Value("${com.tiejun.properties.password}")
 	private String password;
 
 	public String getUserName() {
--- log4j-test/src/test/java/com/tiejun/log4jtest/Log4jTestApplicationTests.java
+++ log4j-test/src/test/java/com/tiejun/log4jtest/Log4jTestApplicationTests.java
@@ -1,5 +1,7 @@
 package com.tiejun.log4jtest;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,12 +26,51 @@ public class Log4jTestApplicationTests {
 	
     @Test
     public void test1() throws Exception {
-        Assert.assertEquals("XiaoMing", properties.getUserName());
-        logger.error(">>>>>>>>>>>>>>>>>> is log error info....");
-        logger.debug(">>>>>>>>>>>>>>>>>> is log debug info....");
-        logger.info(">>>>>>>>>>>>>>>>>> is log info..........");
-        Assert.assertEquals("123456789", properties.getPassword());
+    	testArrayIndexOuputException();
+    	testUserName("XiaoMing");
+    	testPassword("123456789");
+//        Assert.assertEquals("XiaoMing", properties.getUserName());
+//        Assert.assertEquals("123456789", properties.getPassword());
     }
-
+    
+    /**
+     * 数组角标越界测试
+     */
+    public void testArrayIndexOuputException() {
+    	ArrayList<Integer> array = new ArrayList<>();
+    	array.add(1);
+    	array.add(2);
+    	try {
+    		array.get(array.size());
+    	}catch (Exception e) {
+			logger.error(">> The array size is: " + array.size() +" and The index out put", e);
+		}
+    }
+    /**
+     * 用户名测试
+     * @param username
+     */
+    public void testUserName(String username) {
+    	if(null != properties.getUserName()) {
+    		if(username.equals(properties.getUserName())) {
+        		logger.info(">> The user name is: " + properties.getUserName());
+        	}else{
+        		logger.info(">> The user name not equal XiaoMing, The user name is: " + properties.getUserName());
+        	}
+    	}else {
+    		logger.error(">> The system user name is null, place check.");
+    	}
+    }
+    
+    /**
+     * 用户密码测试
+     * @param password
+     */
+    public void testPassword(String password) { 
+    	if(password.equals(properties.getPassword())) {
+    		logger.info(">> The password is: " + properties.getPassword());
+    	}else {
+    		logger.error(">> The password is error, The password is: " + properties.getPassword());
+    	}
+    }
 }
-
